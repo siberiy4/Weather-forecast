e786faf1104682bb0f98e6ce421b56baa5611225
yo-kanyukari <e1n17075@st.oit.ac.jp>
2018-06-09 02:06:55 +0900

    天気と最高気温・最低気温を抜こうとした(ダメ)

--- plugins/whetherbot.py
+++ plugins/whetherbot.py
@@ -4,8 +4,15 @@ import requests
 
 @listen_to('天気')
 def weather(message):
-    city_id = '270000' # 大阪
-    url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + city_id
-    data = requests.get(url).json()
-    text = data['description']['text'].encode()
-    message.send(text)
+	city_id = '2721000' # 大阪
+	url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + city_id
+	data = requests.get(url).json()
+	text = data['forecasts']['0']['dateLabel'].encode()
+	text +=" : "
+	text += data['forecasts']['0']['telop'].encode()
+	text +="\n最低気温："	
+	text += data['forecasts']['0']['temperature']['min']['celsius'].encode()
+	text +="   最高気温："	
+	text += data['forecasts']['0']['temperature']['max']['celsius'].encode()
+	text +="\n"	
+	message.send(text)
