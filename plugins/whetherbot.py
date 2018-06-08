f90d0a44a2b9314deb204456c7c8b762b59d833a
hxs <hxs@HxS-yukari.is.oit.ac.jp>
2018-06-08 19:47:01 +0900

    動くようになった

--- plugins/whetherbot.py
+++ plugins/whetherbot.py
@@ -1,6 +1,8 @@
+from slackbot.bot import listen_to
 import requests
 
-@listen_to('tenki')
+
+@listen_to('天気')
 def weather(message):
     city_id = '270000' # 大阪
     url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + city_id
