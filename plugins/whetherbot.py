00e39bcedbd4247aa9ad8c3542d15262c7afcad6
yo_kanyukari <e1n17075@st.oit.ac.jp>
2018-06-09 18:16:47 +0900

    最高気温最低気温は諦め

--- plugins/whetherbot.py
+++ plugins/whetherbot.py
@@ -11,16 +11,22 @@ def weather(message):
 	# '130010'とすると東京の情報を取得してくれる
 	# ここを変えれば任意の地域の天気情報を取得できる
 	city_id = '270000'
-	html = urllib.request.urlopen(url + city_id)
-	jsonfile = json.loads(html.read().decode('utf-8'))
+	data = urllib.request.urlopen(url + city_id)
+	jsonfile = json.loads(data.read().decode('utf-8'))
 	place=jsonfile['title']
 	state0=jsonfile['forecasts'][0]['telop']
-	maxtemperature0=jsonfile['forecasts'][0]['temperature']['max']['celsius']
+	#maxtemperature0=jsonfile['forecasts'][0]['temperature']['max']['celsius']
 	state1=jsonfile['forecasts'][1]['telop']
-	mintemperature1=jsonfile['forecasts'][1]['temperature']['min']['celsius']
-	maxtemperature1=jsonfile['forecasts'][1]['temperature']['max']['celsius']
+	#mintemperature1=jsonfile['forecasts'][1]['temperature']['min']['celsius']
+	#maxtemperature1=jsonfile['forecasts'][1]['temperature']['max']['celsius']
 
-	text=place+"\n今日の天気："+state0+"   最高気温："+maxtemperature0+"\n明日の天気："+state1+"   最高気温："+maxtemperature1+"   最低気温："+mintemperature1
+#['celsius']を入れると    
+# maxtemperature0=jsonfile['forecasts'][0]['temperature']['max']['celsius'] 
+# TypeError: 'NoneType' object is not subscriptabl
+#とでるので、できない
+
+
+	#text=place+"\n今日の天気："+state0+"   最高気温："+maxtemperature0+"\n明日の天気："+state1+"   最高気温："+maxtemperature1+"   最低気温："+mintemperature1
 	text=place+"\n今日の天気："+state0+"\n明日の天気："+state1
 
 	message.send(text) 
