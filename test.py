e7c68bb4294f7d0e4d855ff61cfdc7be59826687
yo-kanyukari <e1n17075@st.oit.ac.jp>
2018-06-09 13:13:49 +0900

    なんもわからん

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-from slackbot.bot import listen_to
 import requests
-import json
 
-@listen_to('天気')
-def weather(message):
+
+def main():
 	city_id = '2721000' # 大阪
 	url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + city_id
-	data = requests.get(url).json()
+	data = requests.get(url).json
 	print(requests.get(url))
-
+	print(data['forecasts'])
