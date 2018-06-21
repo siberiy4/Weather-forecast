77651adacadd8442678754b8a1f6574ac8565527
yo_kanyukari <e1n17075@st.oit.ac.jp>
2018-06-22 01:08:23 +0900

    時間指定を加えた

---
+++ plugins/schedule.py
@@ -0,0 +1,18 @@
+## 10分ごとに天気を取得し変更する。このファイルを実行する。
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import weatherbot 
+
+# スケジューラー
+sched = BlockingScheduler()
+
+# 10の倍数分に天気を取得しそれぞれ変更する
+
+
+@sched.scheduled_job("cron", hour="6,21",minute)
+def scheduled_job():
+  weatherbot.prediction()
+
+
+# スケジューラー開始
+sched.start()
--- plugins/whetherbot.py
+++ plugins/whetherbot.py
@@ -6,9 +6,11 @@ import datetime
 import time
 
 @listen_to('天気')
+def hanno(message):
+    	weather(message)
+
+
 def weather(message):
-
-
 			url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city='
 			# '130010'とすると東京の情報を取得してくれる
 			# ここを変えれば任意の地域の天気情報を取得できる
@@ -28,7 +30,7 @@ def weather(message):
 					today_max = "None"
 
 			temperature = tomorrow['temperature']
-			if temperature is not "None":
+			if temperature['max'] is not "null":
 					tommorow_temperature = "   最高気温：" + str(temperature['max']['celsius']) +"   最低気温："+ str(temperature['min']['celsius'])
 			else :
 					tommorow_temperature = "気温 : None"
@@ -47,24 +49,10 @@ def weather(message):
 			message.send(text)
 
 
-@listen_to('天気予報')
-def prediction(massage):
-		count = 0
-		yestarday= 17
-		while 1 :
-				
-				nowtime = datetime.datetime.now()
-				today =int(nowtime.day)
-				if today!=yestarday :
-						count = 0
-				if today!=yestarday :
-							yestarday = today
-						
-				now_time_h = int(nowtime.hour)
-				now_time_m = int(nowtime.minute)
-				if count == 0 and 5 <= now_time_h and now_time_h<=6 and 45<=now_time_m and now_time_m<=30 :
-						weather(massage)
-				time.sleep(1800)
+
+
+def prediction(message):
+		weather(message)
 
 
 
