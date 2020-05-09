b8d33725b216b393e315d2ff0750a1f0c0fad73e
yo_kanyukari <e1n17075@st.oit.ac.jp>
2020-05-09 21:32:57 +0900

    Slackに天気予報を送り付ける

--- post_message.py
+++ post_message.py
@@ -1,7 +1,25 @@
-import requests
-import json
+from get_forecast import get_forecast
 
-def post_message(webhookURL):
-    response = requests.post(webhookURL, json.dumps({'text': 'test'}), \
+import requests
+
+def post_message(webhookURL, city_id):
+    place, today, today_gif, today_max, tomorrow, tomorrow_min, tomorrow_max = get_forecast(city_id)
+    
+    if place == None or today==None :
+        message='{"text":"天気予報がわかりませんでした"}'
+        pass
+    else:
+        text = '今日の天気：{} 最高気温: {}'.format(today, today_max)
+        section = text+'\n明日の天気:{} 最低気温: {} 最高気温: {}'.format(
+            tomorrow, tomorrow_min, tomorrow_max)
+        if today_gif == None:
+            message = '{"text":"{}"}'.format(section)
+        else:
+            message = '{"text":"{}","blocks": [{"type": "section","text": {"type": "mrkdwn","text": "<!channel>\n{}"},\
+                "accessory": {"type": "image","image_url": "{}","alt_text": "weather"}}]}'.format(text, section, today_gif)
+            pass
+        pass
+
+    response = requests.post(webhookURL, message, \
         headers={'Content-Type': 'application/json'})
     print(response)
